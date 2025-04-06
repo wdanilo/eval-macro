f3ae6d2c312375ddd4a69aa3bed86d1317e60e31
Wojciech Danilo <wojciech.danilo@gmail.com>
2025-04-07 01:57:11 +0200

    Fixes #32

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -49,7 +49,7 @@ const OUT_DIR: &str = env!("OUT_DIR");
 const KEYWORDS: &[&str] = &[
     "as", "async", "await", "break", "const", "continue", "crate", "dyn", "else", "enum",
     "extern", "false", "fn", "for", "if", "impl", "in", "let", "loop", "match", "mod", "move",
-    "mut", "pub", "ref", "return", "self", "Self", "static", "struct", "super", "trait", "true",
+    "mut", "pub", "ref", "return", "self", "Self", "struct", "super", "trait", "true",
     "type", "unsafe", "use", "where", "while", "abstract", "become", "box", "do", "final", "macro",
     "override", "priv", "typeof", "unsized", "virtual", "yield", "try",
 ];
