bec37979174df4eee2abd5aa4a4b70059fff1e4f
Mattia Pitossi <mattia.pitossi@moneyfarm.com>
2025-06-09 12:55:05 +0200

    feat: introduce examples

---
+++ examples/gen_struct/src/main.rs
@@ -0,0 +1,27 @@
+#[crabtime::function]
+fn gen_struct(components: Vec<String>) {
+    let fields = components
+        .iter()
+        .map(|x| format!("{}: &'static str", x))
+        .collect::<Vec<String>>()
+        .join(",");
+
+    crabtime::output! {
+        #[derive(Debug)]
+        struct User {
+            {{fields}}
+        }
+    }
+}
+
+fn main() {
+    gen_struct!(["name", "last_name", "birthdate"]);
+
+    let data = User {
+        name: "John",
+        last_name: "Doe",
+        birthdate: "01/01/1970",
+    };
+
+    println!("{}", data.name)
+}
